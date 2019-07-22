90b60055254bd97296425dfcc4db6a50ebfe4fb3
Devon Marantz <DMarantz@talentpath.com>
2019-07-22 12:18:51 -0700

    Finish Your Own Loop

--- chapter5HOF/5-2_Your-Own-Loop.js
+++ chapter5HOF/5-2_Your-Own-Loop.js
@@ -1,5 +1,11 @@
 // Your code here.
-const loop = () => {};
+const loop = (num, test, update, func) => {
+  let value = num;
+  while (test(value)) {
+    func(value);
+    value = update(value);
+  }
+};
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
